3fa7a2f5f85be4510c76ad6378c9db01526474c3
mrsantokhs <mrsantokhs@gmail.com>
2021-09-10 00:45:49 +1200

    Cloud Pipeline Change

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Hello Cloud!"));
+app.get("/", (req, res) =>
+  res.send("Hello Cloud Pipeline and Elastic Beanstalk!")
+);
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
